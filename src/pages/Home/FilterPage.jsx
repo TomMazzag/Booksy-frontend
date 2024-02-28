30eaefc765a48261faca2a91739e609281fbc702
Jehoshua <137894662+AVJehoshua@users.noreply.github.com>
2024-02-28 16:48:12 +0000

    Created filters.js service to get filtered books

--- src/pages/Home/FilterPage.jsx
+++ src/pages/Home/FilterPage.jsx
@@ -3,45 +3,59 @@
 // import { Checkbox } from "@/components/CheckBox"
 import Navbar from "../../components/Home/NavBar.jsx";
 import Footer from "../../components/Home/Footer.jsx";
-import "./FilterPage.css"
-import "../../components/Home/Footer.css";
-import { CheckBox } from "../../components/Home/CheckBox.jsx";
+import { CheckBox } from "../../components/Home/Filter/CheckBox.jsx";
+import { SortBy } from "../../components/Home/Filter/Sort.jsx";
 
+import "../../components/Home/Footer.css";
+import "./FilterPage.css"
+import { getAllBooks } from "../../services/Filters.js";
 export const FilterPage = () => {
 
 
     const categories = [
-        "Young Adult",
+        // "Young Adult",
         "Romance",
         "Action",
         "History",
-        "Non-fiction",
+        // "Non-fiction",
         "Thriller",
-        "LGBTQIA+",
+        // "LGBTQIA+",
         "Mystery",
         "Autobiography"
     ];
-    // const renderCategories = () => {
-    //     return 
-    // };
+
+
+    const clickButton = async () => {
+        try {
+            await getAllBooks(categories)
+            .then((books) => {
+                console.log(books)
+            }) 
+        }
+        catch(error) {
+            console.log("failed to fetch")
+        }
+    }
+    
 
 
 
     return (
         <>
         <Navbar />
-        <div className="filter-page-title"> Categories: </div>
+        <div className="filter-page-title"> Categories</div>
+        <SortBy />
+        <div className="categories-box">
         <div className="categories-container">
+                <h3 className="categories-title">Categories List:</h3>
+                <br></br>
                 {categories.map((category, index) => (
                     <CheckBox key={ index } categories={ category }/>
-            ))}
+                    ))}
         </div>
+        </div>
+        <button onClick={clickButton}>Click me </button>
         <Footer />
         </>
     )
 }
-
-
-
-
-
