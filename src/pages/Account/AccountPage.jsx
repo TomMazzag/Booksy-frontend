60d82bab0c793e899ace5f50cc79fa0754c93c7b
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-03-05 13:15:49 +0000

    added tabs and css

--- src/pages/Account/AccountPage.jsx
+++ src/pages/Account/AccountPage.jsx
@@ -3,23 +3,31 @@
 import React, { useEffect, useState } from 'react';
 import { useUser } from '@clerk/clerk-react';
 import Navbar from "../../components/Home/NavBar.jsx";
-import BookCard from "../../components/Home/BookCard.jsx";
 import Footer from "../../components/Home/Footer.jsx";
-import "../Home/HomePage.css";
+import "../Account/AccountPage.css"
 
 export const AccountPage = () => {
-    const [userData, setuserData] = useState([]);
-    const { isSignedIn, user } = useUser();
+    const [currentTab, setCurrentTab] = useState('Account');
+    const tabs = ['Account', 'Security', 'Order History', 'Emails', 'Privacy'];
+    const { user } = useUser();
 
     return (
         <>
             <Navbar />
-            <div className="title">
-                <h1>Your Account</h1>
-            </div>
-            <div className="category-grid-container">
-                <div className="category-grid">
-                    <p>User fullname: {user?.fullName}</p>
+            <div className="account-container">
+                <div className="tabs">
+                    {tabs.map(tab => (
+                        <button
+                            key={tab}
+                            className={`tab ${currentTab === tab ? 'active' : ''}`}
+                            onClick={() => setCurrentTab(tab)}
+                        >
+                            {tab}
+                        </button>
+                    ))}
+                </div>
+                <div className="tab-content">
+                    <p>{currentTab}</p>
                 </div>
             </div>
             <Footer />
