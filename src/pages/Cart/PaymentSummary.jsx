877829f23f283635d76420d5450df138c656c1c1
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-03-06 17:15:21 +0000

    removed image usages

--- src/pages/Cart/PaymentSummary.jsx
+++ src/pages/Cart/PaymentSummary.jsx
@@ -7,22 +7,22 @@ const PaymentSummary = ({ totalPrice }) => {
             <form action="#">
                 <label htmlFor="applePay">
                     <input type="radio" id="applePay" name="payment-method" value="applePay" />
-                    <img src={applePayLogo} alt="Apple Pay" />
+                    {/* <img src={applePayLogo} alt="Apple Pay" /> */}
                 </label><br />
 
                 <label htmlFor="visaMastercard">
                     <input type="radio" id="visaMastercard" name="payment-method" value="visaMastercard" />
-                    <img src={visaMastercardLogo} alt="Visa/Mastercard" />
+                    {/* <img src={visaMastercardLogo} alt="Visa/Mastercard" /> */}
                 </label><br />
 
                 <label htmlFor="paypal">
                     <input type="radio" id="paypal" name="payment-method" value="paypal" />
-                    <img src={paypalLogo} alt="PayPal" />
+                    {/* <img src={paypalLogo} alt="PayPal" /> */}
                 </label><br />
 
                 <label htmlFor="gpay">
                     <input type="radio" id="gpay" name="payment-method" value="gpay" />
-                    <img src={gpayLogo} alt="Google Pay" />
+                    {/* <img src={gpayLogo} alt="Google Pay" /> */}
                 </label><br />
 
                 <p>Item(s) total: £{totalPrice.toFixed(2)}</p>
