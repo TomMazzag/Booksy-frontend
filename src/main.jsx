9bfa71a38d3e5609627c17d5982f4656caee8686
ilhan <ilhanabdalle@gmail.com>
2024-03-07 14:14:18 +0000

    spacing issue on basket page fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
